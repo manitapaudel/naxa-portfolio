f22d0b63e44326c3ad055ad1f9c6f6b9d930b5bf
Manita Paudel <paudel04manita@gmail.com>
2024-08-22 17:28:47 +0545

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="bg-primary">Hello there!</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffdc1c",
+        secondary: "#124af4",
+      },
+    },
+  },
+  plugins: [],
+};
