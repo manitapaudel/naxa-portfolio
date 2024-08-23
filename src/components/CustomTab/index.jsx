fde5f95a409b9e6784cfe4192bc74146509406b1
Manita Paudel <paudel04manita@gmail.com>
2024-08-23 22:11:06 +0545

    Align items center in the grid

--- src/components/CustomTab/index.jsx
+++ src/components/CustomTab/index.jsx
@@ -17,7 +17,7 @@ const CustomTab = ({ children }) => {
 
   return (
     <div className="container mx-auto  ">
-      <div className="grid grid-cols-5 gap-10 bg-white py-2.5 px-7.5 -mt-12 shadow-2xl">
+      <div className="grid grid-cols-5 gap-10 items-center bg-white py-2.5 px-7.5 -mt-12 shadow-2xl">
         <Link
           to="keyhighlights"
           className={`max-h-max hover:bg-primary text-secondary font-semibold p-2 ${
