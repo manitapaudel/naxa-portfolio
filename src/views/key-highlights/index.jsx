87de72d7f0d52c52e7943fe05ea59368a96e4fe3
Manita Paudel <paudel04manita@gmail.com>
2024-08-25 11:02:29 +0545

    Sort the key highlights to show as per the original page

--- src/views/key-highlights/index.jsx
+++ src/views/key-highlights/index.jsx
@@ -5,11 +5,17 @@ import HighlightCard from "@/components/HighlightCard";
 const KeyHighlights = () => {
   const { keyHighlights } = useSelector((state) => state.projects);
 
+  const sortedHighlights = [...keyHighlights].sort((a, b) => {
+    const aDate = new Date(a.end_date);
+    const bDate = new Date(b.end_date);
+    return bDate - aDate; // the project with latest end_date comes first in the list
+  });
+
   return (
     <>
       <div className="pb-20">
         <div className="grid grid-cols-2 gap-y-10 items-center">
-          {keyHighlights.map((highlight) => (
+          {sortedHighlights.map((highlight) => (
             <HighlightCard highlight={highlight} key={highlight.id} />
           ))}
         </div>
